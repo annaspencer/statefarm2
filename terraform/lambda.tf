b287175dd28032717362d3bb4ea68ac7c0d79b54
AbigayiBrown <abbrown1@email.arizona.edu>
2020-10-28 16:37:47 -0700

    Let's see if this works

---
+++ terraform/lambda.tf
@@ -0,0 +1,102 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+}
+
+provider "aws" {
+   region = "us-west-2"
+   versio = "~>2.0"
+}
+
+resource "aws_lambda_function" "hello" {
+   function_name = "Serverlesshello"
+
+   # The bucket name as created earlier with "aws s3api create-bucket"
+   s3_bucket = "/terraform-serverless-hello"
+   s3_key    = "v1.0.0/hello.zip"
+
+   # "main" is the filename within the zip file (main.js) and "handler"
+   # is the name of the property under which the handler function was
+   # exported in that file.
+   handler = "main.handler"
+   runtime = "nodejs10.x"
+
+   role = aws_iam_role.demo_2020.arn
+}
+
+ # IAM role which dictates what other AWS services the Lambda function
+ # may access.
+resource "aws_iam_role" "demo_2020" {
+   name = "serverless_hello_lambda"
+
+   assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+
+}
+
+resource "aws_api_gateway_resource" "proxy" {
+   rest_api_id = aws_api_gateway_rest_api.hello.id
+   parent_id   = aws_api_gateway_rest_api.hello.root_resource_id
+   path_part   = "{proxy+}"
+}
+
+resource "aws_api_gateway_method" "proxy" {
+   rest_api_id   = aws_api_gateway_rest_api.hello.id
+   resource_id   = aws_api_gateway_resource.proxy.id
+   http_method   = "ANY"
+   authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "lambda" {
+   rest_api_id = aws_api_gateway_rest_api.hello.id
+   resource_id = aws_api_gateway_method.proxy.resource_id
+   http_method = aws_api_gateway_method.proxy.http_method
+
+   integration_http_method = "POST"
+   type                    = "AWS_PROXY"
+   uri                     = aws_lambda_function.hello.invoke_arn
+}
+
+resource "aws_api_gateway_method" "proxy_root" {
+   rest_api_id   = aws_api_gateway_rest_api.hello.id
+   resource_id   = aws_api_gateway_rest_api.hello.root_resource_id
+   http_method   = "ANY"
+   authorization = "NONE"
+}
+
+resource "aws_api_gateway_integration" "lambda_root" {
+   rest_api_id = aws_api_gateway_rest_api.hello.id
+   resource_id = aws_api_gateway_method.proxy_root.resource_id
+   http_method = aws_api_gateway_method.proxy_root.http_method
+
+   integration_http_method = "POST"
+   type                    = "AWS_PROXY"
+   uri                     = aws_lambda_function.hello.invoke_arn
+}
+
+resource "aws_lambda_permission" "apigw" {
+   statement_id  = "AllowAPIGatewayInvoke"
+   action        = "lambda:InvokeFunction"
+   function_name = aws_lambda_function.hello.function_name
+   principal     = "apigateway.amazonaws.com"
+
+   # The "/*/*" portion grants access from any method on any resource
+   # within the API Gateway REST API.
+   source_arn = "${aws_api_gateway_rest_api.hello.execution_arn}/*/*"
+}
