80a3c8b7bfbf7513ab397c1850200d2ce73f853e
AbigayiBrown <abbrown1@email.arizona.edu>
2020-11-05 17:16:36 -0700

    last minute fix

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -20,6 +20,14 @@ variable "role_name" {
   default     = "demo_terraform_2020"
 }
 
+# This must be a unique name
+# The AWS_S3_ZIP_FILE in the .gitlab-ci.yml must match the role here.
+variable "s3_key" {
+  type        = string
+  description = "The key location for the serverless package"
+  default     = "v1.0.0/hello.zip"
+}
+
 ################# DO NOT CHANGE ANY OF THE VARIABLES BELOW THIS LINE #################
 
 variable "region" {
@@ -44,10 +52,4 @@ variable "s3_bucket" {
   type        = string
   description = "The S3 bucket that holds the packaged function"
   default     = "2020-terraform-demo"
-}
-
-variable "s3_key" {
-  type        = string
-  description = "The key location for the serverless package"
-  default     = "v1.0.0/hello.zip"
 }
--- terraform/variables.tfvars
+++ terraform/variables.tfvars
@@ -6,7 +6,7 @@ describe_function = "Describe me."
 role_name = "demo_terraform_2020"
 
 # This must be a unique name
-# The AWS_DEFAULT_ROLE in the .gitlab-ci.yml must match the role here.
+# The AWS_S3_ZIP_FILE in the .gitlab-ci.yml must match the role here.
 s3_key = "v1.0.0/hello-world.zip"
 
 ################# DO NOT CHANGE ANY OF THE VARIABLES BELOW THIS LINE #################
